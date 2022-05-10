1e1370dbe73fb42bf0575b801e8a4bb9f63cbe51
Natheus <natheusmunes@gmail.com>
2022-05-10 18:32:00 -0300

    add create e update hero

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -28,8 +28,34 @@ export class HeroService {
     return this.http
       .get<Hero>(`${this.heroesUrl}/${id}`)
       .pipe(
-        tap((hero) => this.log(`fetched hero id=${id} and name=${hero.name}`))
+        tap((hero) => this.log(`fetched ${this.descAttributes(hero)}`))
       );
+  }
+
+  // POST /heroes
+  createHero(hero: Hero): Observable<Hero> {
+    return this.http
+    .post<Hero>(this.heroesUrl, hero)
+    .pipe(
+      tap((hero) =>
+        this.log(`create ${this.descAttributes(hero)}`)
+      )
+    )
+  }
+
+  // PUT /heroes/id
+  updateHero(hero: Hero): Observable<Hero> {
+    return this.http
+      .put<Hero>(`${this.heroesUrl}/${hero.id}`, hero)
+      .pipe(
+        tap((hero) =>
+          this.log(`updated ${this.descAttributes(hero)}`)
+        )
+      );
+  }
+
+  private descAttributes(hero: Hero): string {
+    return `Hero ID=${hero.id} and Name=${hero.name}`;
   }
 
   private log(message: string): void {
--- src/app/heroes/components/hero-detail/hero-detail.component.ts
+++ src/app/heroes/components/hero-detail/hero-detail.component.ts
@@ -11,6 +11,7 @@ import { Hero } from '../../../core/models/hero.model';
 })
 export class HeroDetailComponent implements OnInit {
   hero!: Hero;
+  isEditing!: boolean;
 
   constructor(
     private heroService: HeroService,
@@ -23,12 +24,31 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const paramId = this.route.snapshot.paramMap.get('id');
+    if (paramId === 'new') {
+      this.isEditing = false;
+      this.hero = { name: '' } as Hero;
+    } else {
+      this.isEditing = true;
 
-    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+      const id = Number(paramId);
+      this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+    }
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  isFormValid(): boolean {
+    return !!this.hero.name.trim();
+  }
+
+  create(): void {
+    this.heroService.createHero(this.hero).subscribe(() => this.goBack());
+  }
+
+  update(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
